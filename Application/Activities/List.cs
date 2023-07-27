1c73034fe5cfce4772522b5f8f40742b47f8de59
asmitaupreti <asmitaupreti52@gmail.com>
2023-07-27 14:58:34 +0200

    Warpup section 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -15,31 +15,16 @@ namespace Application.Activities
         {
            
             public readonly ApplicationDbContext _db;
-            public readonly ILogger<List> _logger ;
+           
 
             public Handler(ApplicationDbContext db, ILogger<List> logger)
             {
-                _logger= logger;
+                
                 _db=db;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000,cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    _logger.LogInformation($"Task was cancelled");
-                    
-                }
-
                 return await _db.Activities.ToListAsync();
             }
         }
